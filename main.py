e8d54ab7ee214b7cf405790033abb5a5a1c32aca
RealOrxify <145070273+RealOrxify@users.noreply.github.com>
2023-10-17 11:55:50 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+
+# PlutoSphere API URL
+api_url = "https://api.plutosphere.com/"
+
+# User credentials
+username = "your_username"
+password = "your_password"
+
+# Token amount to add
+token_amount = 100
+
+# Login to PlutoSphere
+login_data = {"username": username, "password": password}
+login_response = requests.post(api_url + "auth/login", json=login_data)
+
+if login_response.status_code == 200:
+    # Get user ID from login response
+    user_id = login_response.json()["user"]["id"]
+
+    # Add tokens to user account
+    add_tokens_data = {"userId": user_id, "amount": token_amount}
+    add_tokens_response = requests.post(api_url + "users/addTokens", json=add_tokens_data)
+
+    if add_tokens_response.status_code == 200:
+        print("Tokens added successfully.")
+    else:
+        print("Error adding tokens:", add_tokens_response.status_code)
+else:
+    print("Login failed:", login_response.status_code)
